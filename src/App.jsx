d00920e23d4799e266b26e9728e07f3ed46e92c5
Adrian Rebmann <rebmann@informatik.uni-mannheim.de>
2024-07-17 09:36:29 +0200

    fix layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
 import CheckingWizard from './components/CheckingWizard';
 import { Route, Routes, useNavigate } from 'react-router-dom'; 
 
+import {
+  Button,
+  DynamicPage,
+  DynamicPageTitle,
+  Title,
+  Label,
+  Badge,
+  WrappingType,
+} from '@ui5/webcomponents-react';
+
 import './App.css'
 
 const App = () => {
@@ -8,6 +18,30 @@ const App = () => {
 
   return (
     <>
+          <DynamicPage
+        alwaysShowContentHeader
+        style={{
+          position: 'fixed',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+        }}
+        showHideHeaderButton={false}
+        headerContentPinnable={false}
+        headerTitle={
+          <DynamicPageTitle
+            header={<Title>Best-Practice Checker</Title>}
+            subHeader={
+              <>
+                <Label>
+                  Checking Best-Practice Violations in Event Logs
+                </Label>
+              </>
+            }
+          ></DynamicPageTitle>
+        }
+      >
         <Routes>
           <Route
             path="/"
@@ -16,6 +50,7 @@ const App = () => {
             }
           />
         </Routes>
+        </DynamicPage>
     </>
   )
 }
